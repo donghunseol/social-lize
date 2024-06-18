fde8ba75fcbedca162fb42282ca472dd328e7bb9
egdg4587 <egdg7777@gmail.com>
2024-06-18 15:46:23 +0900

    소셜멤버 더미

--- src/main/java/com/example/project/_core/enums/SocialMemberStateEnum.java
+++ src/main/java/com/example/project/_core/enums/SocialMemberStateEnum.java
@@ -2,6 +2,6 @@ package com.example.project._core.enums;
 
 public enum SocialMemberStateEnum {
 
-    // 상태(MEMBER : 일반 멤버, MANAGER : 관리자)
-    MEMBER, MANAGER
+    // 상태(APPROVED : 가입 승인, REFUSE : 가입 거절, WAITING : 가입 대기중, RESIGN : 소셜 강퇴)
+    APPROVED, REFUSE, WAITING, RESIGN
 }
