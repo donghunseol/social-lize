372032715abd6a70f957c23803ff3ed220deba5a
egdg4587 <egdg7777@gmail.com>
2024-06-20 18:15:10 +0900

    게시글 쓰기 도중

--- src/main/java/com/example/project/board/BoardRequest.java
+++ src/main/java/com/example/project/board/BoardRequest.java
@@ -1,17 +1,22 @@
 package com.example.project.board;
 
 import com.example.project._core.enums.BoardEnum;
+import com.example.project.file.File;
 import com.example.project.social.Social;
 import com.example.project.user.User;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public class BoardRequest {
 
     @Data
     public static class SaveDTO {
         private String content;
+        private List<MultipartFile> imgFiles;
+        private List<MultipartFile> videoFiles;
 
         public Board toEntity(Social socialId, User userId) {
             return Board.builder()
@@ -19,6 +24,19 @@ public class BoardRequest {
                     .socialId(socialId)
                     .content(content)
                     .role(BoardEnum.POST)
+                    .build();
+        }
+    }
+
+    @Data
+    public static class FileDTO {
+        private String path;
+
+        public File toEntity(Social socialId, Board boardId, User userId) {
+            return File.builder()
+                    .socialId(socialId)
+                    .boardId(boardId)
+                    .userId(userId)
                     .build();
         }
     }
