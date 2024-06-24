71a140a2f76603b631a26a8f244b2f961798bf92
sw-kim <kimsaewoon@gmail.com>
2024-06-24 10:27:35 +0900

    Redis Docker설정 파일

--- src/main/java/com/example/project/notification/NotificationController.java
+++ src/main/java/com/example/project/notification/NotificationController.java
@@ -1,12 +1,20 @@
 package com.example.project.notification;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class NotificationController {
     private final NotificationService notificationService;
     private final HttpSession session;
+
+    //회원가입 페이지 - 자체가입
+    @GetMapping("/notiTest")
+    public String notiTest(){
+        return "notificationTest/test";
+    }
 }
