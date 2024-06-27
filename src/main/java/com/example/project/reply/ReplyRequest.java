c7d82dddd0136843080db28238f86e07c96fc885
test231212 <j910219@gmail.com>
2024-06-27 17:54:13 +0900

    댓글 진행중

--- src/main/java/com/example/project/reply/ReplyRequest.java
+++ src/main/java/com/example/project/reply/ReplyRequest.java
@@ -1,4 +1,24 @@
 package com.example.project.reply;
 
+import com.example.project.board.Board;
+import com.example.project.user.User;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
 public class ReplyRequest {
+
+    @Data
+    public static class SaveDTO {
+        private Integer boardId;
+        private String comment;
+
+        public Reply toEntity(User sessionUser, Board board){
+            return Reply.builder()
+                    .userId(sessionUser)
+                    .boardId(board)
+                    .comment(comment)
+                    .build();
+        }
+    }
 }
