e4ca905a90e36c00716be31800f75f1704e2ec16
tkffkels93 <tkffkels93@gmail.com>
2024-06-28 11:30:30 +0900

    관리자 / 소셜 강제 삭제

--- src/main/java/com/example/project/social/SocialRepository.java
+++ src/main/java/com/example/project/social/SocialRepository.java
@@ -4,15 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SocialRepository extends JpaRepository<Social, Integer> {
 
-    // 활동 소셜 리스트 조회 (관리자)
     @Query("select s from Social s where s.name = :name")
     Optional<Social> findByName(@Param("name") String name);
 
     // 활동 소셜 갯수 조회 (관리자)
     @Query("select count(*) from Social s where s.status = 'ACTIVE'")
     Integer findAllActiveSocial();
+
+    // 활동 소셜 리스트 조회 (관리자)
+    @Query("select s from Social s where s.status = 'ACTIVE'")
+    List<Social> findAllActiveSocialList();
 }
